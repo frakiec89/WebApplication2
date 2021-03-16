a5fa796037abe1b79b36df83f636ee8e70df0d75
РИШАТ Ахтямов <rishat-rus@mail.ru>
2021-03-16 23:09:41 +0400

    Добавьте файлы проекта.

---
+++ WebApplication2/Class.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2
+{
+    public class  POSTin
+    {
+        public string patient { get; set; }
+        public List<service> services { get; set; }
+
+    }
+
+    public class GETiout
+    {
+        public string patient { get; set; }
+        public List<service> services { get; set; }
+
+    }
+
+    public class service
+    {
+        public int сode { get; set; }
+        public string result { get; set; }
+    }
+
+    public class GETStatus
+    {
+        public int progress { get; set; }
+    }
+}
---
+++ WebApplication2/Controllers/AnalyzerController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Threading;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/Analyzer/[controller]")]
+    [ApiController]
+    public class LedetectController : ControllerBase
+    {
+        private static GETiout _out = new GETiout();
+
+
+
+        [HttpPost]
+        public ObjectResult InPost(POSTin pOSTin)
+        {
+            if (pOSTin is null)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, "dmskdms");
+            }
+
+            if (pOSTin.patient == null)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, "dmskdms");
+            }
+
+            if (pOSTin.services == null)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, "dmskdms");
+            }
+
+            Random random = new Random();
+
+            _out = null;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(random.Next(100, 500));
+            }
+
+
+            _out = new GETiout() { patient = pOSTin.patient, services = pOSTin.services };
+
+            foreach (var item in _out.services)
+            {
+                item.result = "good";
+            }
+
+
+            return StatusCode(StatusCodes.Status200OK, "ok" );
+          
+        }
+
+        [HttpGet]
+        public ObjectResult InPost()
+        {
+
+            if (_out != null)
+              return StatusCode(StatusCodes.Status200OK, _out);
+
+            return StatusCode(StatusCodes.Status400BadRequest, "Анализатор занят");
+
+        }
+    }
+    }
+
+    
+
